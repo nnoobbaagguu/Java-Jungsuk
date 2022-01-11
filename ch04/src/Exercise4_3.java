771d6a4c6160666dc5edd24e84949bbb4d924119
nnoobbaagguu <97185888+nnoobbaagguu@users.noreply.github.com>
2022-01-12 00:26:24 +0900

    Add files via upload

--- ch04/src/Exercise4_3.java
+++ ch04/src/Exercise4_3.java
@@ -1,12 +1,14 @@
 public class Exercise4_3 {
     public static void main(String[] args) {
         int sum = 0;
+        int total_sum = 0;
 
         for (int i = 1; i <= 10; i++) {
-            sum += sum + i;
+            sum = sum + i;
+            total_sum += sum;
         }
 
-        System.out.println(sum);
+        System.out.println(total_sum);
     }
 }
 
